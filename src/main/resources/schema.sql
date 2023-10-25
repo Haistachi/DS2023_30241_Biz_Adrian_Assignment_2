eff90a972e9ff65a720c6d0c62a26b1ad430d61d
Haistachi <baz_adi83@yahoo.com>
2023-10-25 14:02:24 +0300

    First stage update.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,31 +1,14 @@
-/*
 --
 -- TOC entry 212 (class 1259 OID 16503)
 -- Name: user; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE IF NOT EXISTS public."user" (
-                               id int NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS public."person" (
+                               id SERIAL PRIMARY KEY,
                                role character varying(255),
                                user_password character varying(255),
                                username character varying(255)
 );
 
+SELECT setval('person_id_seq', 11);
 ALTER TABLE IF EXISTS public."user" OWNER TO postgres;
-
---
--- TOC entry 3182 (class 2606 OID 16509)
--- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public."user"
-    DROP CONSTRAINT IF EXISTS user_pkey;
-ALTER TABLE ONLY public."user"
-    ADD CONSTRAINT user_pkey PRIMARY KEY (id);
-
--- Completed on 2022-12-17 20:49:15
-
---
--- PostgreSQL database dump complete
---
-*/
