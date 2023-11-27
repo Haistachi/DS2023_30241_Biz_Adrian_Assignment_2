85ab19646c5424775d5a5099bf60278a88400279
Haistachi <baz_adi83@yahoo.com>
2023-11-27 13:21:59 +0200

    comit

--- src/main/java/Send.java
+++ src/main/java/Send.java
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -7,61 +8,72 @@ import com.rabbitmq.client.ConnectionFactory;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
 
 public class Send {
 
-    private final static String QUEUE_NAME = "csvqueue1";
+    private final static String QUEUE_NAME = "demoqueue";
+    public static LocalDateTime time=LocalDateTime.of(2023, 01,17, 1,0,0);
+    public static ArrayList<Double> senzor;
 
-    private static Integer index = 0;
-
-
-    public static List<Double> readCSV() {
-        Scanner sc = null;
-        try {
-            sc = new Scanner(new File("E:\\Facultate\\UTCN\\An4\\DS\\CSV_Reader\\src\\main\\resources\\sensor.csv"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        List<Double> readValues = new ArrayList<>();
-        while (sc.hasNext()) {
-            readValues.add(sc.nextDouble());
+    public static void readFile() throws FileNotFoundException {
+        Scanner sc= new Scanner(new File("./sensor.csv"));
+        sc.useDelimiter("\n");
+        senzor=new ArrayList<Double>();
+        while(sc.hasNext())
+        {
+            String s=sc.next();
+            senzor.add(Double.parseDouble(s));
+            //System.out.println(s);
         }
         sc.close();
-        return readValues;
     }
 
     public static void main(String[] argv) throws Exception {
+
+        readFile();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setUri("amqps://bxjxjwnv:OO07BLJPPe2L3Y7O17WoZXgl1KgmEAgv@goose.rmq2.cloudamqp.com/bxjxjwnv");
+        factory.setUri("amqps://qkhxwxxk:0ji0_18OgB8CqdfSjAT0dOVC_nIm-MUn@kangaroo.rmq.cloudamqp.com/qkhxwxxk");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            for (int x = 1; x <= senzor.size(); x++) {
+                Measurement m = new Measurement();
+                m.setIdMeasurement(x);
+                m.setTimest(time);
+                m.setVal(senzor.get(x-1));
+                m.setIdDevice(Integer.parseInt(argv[0]));
+                String json = objectMapper.writeValueAsString(m);
+                System.out.println(json);
 
-        List<Double> readValues = readCSV();
-        Boolean ok=true;
+                /*QUEUE_NAME: Numele cozii.
+                true: Durable - indică dacă coada trebuie să fie durabilă sau nu. O coadă durabilă supraviețuiește repornirii serverului RabbitMQ.
+                false: Exclusive - indică dacă coada este exclusivă pentru conexiunea curentă sau nu. Dacă este exclusivă, coada va fi ștearsă după ce conexiunea se închide.
+                false: Auto-delete - indică dacă coada trebuie să fie ștearsă automat atunci când nu mai are consumatori sau nu. Dacă este setat la true,
+                coada va fi ștearsă atunci când ultimul consumator se dezabonează.
+                null: Arguments - Aceasta permite specificarea unor argumente suplimentare la declararea cozii. În acest caz, nu sunt specificate argumente suplimentare.*/
+                channel.queueDeclare(QUEUE_NAME, true, false, false, null);
 
-        while(ok){
 
-            try (Connection connection = factory.newConnection();
-                 Channel channel = connection.createChannel()) {
-
-                for (Integer i=0;i<readValues.size();i++ ) {
-
-                    Message msg = new Message();
-                    msg.setReadingDate(new Date());
-                    msg.setIdDevice(Integer.valueOf(argv[0]));
-                    msg.setValue(readValues.get(i));
-                    i++;
-
-                    ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-                    String json = ow.writeValueAsString(msg);
-
-                    channel.queueDeclare(QUEUE_NAME, true, false, false, null);
-                    channel.basicPublish("", QUEUE_NAME, null, json.getBytes(StandardCharsets.UTF_8));
-                    System.out.println(msg);
-                    Thread.sleep(60000);
+                /*                "": Exchange - Numele schimbului. În cazul "", mesajele sunt publicate direct în coada specificată (se numește "default exchange").
+                QUEUE_NAME: Numele cozii către care se trimite mesajul.
+                null: BasicProperties - Aici puteți specifica proprietăți suplimentare ale mesajului, cum ar fi tipul de conținut, expirarea etc.
+                Aici, aceasta este setată la null, ceea ce înseamnă că se folosesc valorile implicite.
+                json.getBytes(StandardCharsets.UTF_8): Body - Conținutul mesajului, convertit în bytes.
+                În acest exemplu, se trimite un mesaj JSON, iar conținutul acestuia este convertit în bytes folosind codificarea UTF-8.*/
+                channel.basicPublish("", QUEUE_NAME, null, json.getBytes(StandardCharsets.UTF_8));
+                try {
+                    Thread.sleep(20000);  // pauza
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    time=time.plusHours(1);
                 }
-                ok=false;
             }
         }
     }
-
 }
